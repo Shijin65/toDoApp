2660ac07d4207a90b29e1893ec849e30f626765b
shijin65 <shijincht65@gmail.com>
2023-07-04 07:10:56 +0530

    mapping is added to the plus icon

--- src/App.js
+++ src/App.js
@@ -20,18 +20,29 @@ function App() {
         value={toDo}
         onChange={(e) => {setToDo(e.target.value)}}
         placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+
+        <i onClick={()=>setToDos([...toDos,toDo])}  className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        <div className="todo">
+        {
+          toDos.map((value)=>{
+
+            return(
+      <div className="todo">
+
           <div className="left">
             <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
+            <p>{value}</p>
           </div>
           <div className="right">
             <i className="fas fa-times"></i>
           </div>
         </div>
+            )
+          })
+        }
+        
+
       </div>
     </div>
     )

152ae727806cf93224a3b4055a89d66148ac59fa
shijin65 <shijincht65@gmail.com>
2023-07-28 12:31:47 +0530

    the UI is changed ,added completed todo page,added deletd todo page

--- src/App.js
+++ src/App.js
@@ -5,83 +5,183 @@ function App() {
   const [toDos, setToDos] = useState([]);
   const [toDo, setToDo] = useState("");
 
+
+
+  var currentdate = new Date(); 
+
+  var datetime    =" @" + currentdate.getHours() + ":"  
+                  + currentdate.getMinutes() + ":" 
+                  + currentdate.getSeconds();
+
+  const handleadd =()=>{if (toDo.length!==0) {
+                    setToDos([...toDos, { id: Date.now(), text: toDo, state:true,del:true ,time:0 ,cm:false }])
+                    }}
+
+
+
   return(
-    <div className="app">
-      
+<div className="app">
       <div className="mainHeading">
         <h1>ToDo List</h1>
       </div>
-      <div className="subHeading">
-        <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
-      </div>
-      <div className="input">
-        <input
-          id={"my-input"}
-          type={"text"}
-          value={toDo}
-          onChange={(e) => {
-            setToDo(e.target.value);
-          }}
-          placeholder="🖊️ Add item..."
-        />
+    <div className="mainclass">
 
-        <i
-          onClick={() =>
-            setToDos([...toDos, { id: Date.now(), text: toDo, tick: false }])
-          }
-          className="fas fa-plus"
-          ></i>
-      </div>
+          {/* < ------------ CREATE TODOS ---------- > */}
+
+      <div className="card">
+        <h2>CREATE A TODO</h2>
+      
+        <div className="input">
+
+          <input
+            id={"my_input"}
+            type={"text"}
+            value={toDo}
+            onChange={(e) => {
+            setToDo(e.target.value);
+            }}
+            placeholder="🖊️ Add item..."
+          />
+
+          <i
+            onClick={handleadd}
+            className="fas fa-plus"
+          />
+      </div> 
+              
+
+
+        {/* < ------------ TODOS LIST ---------- > */}
+
+      <h4 style={{marginTop:'10px'}}>TODO LIST</h4>
       <div className="todos-container">
-        <div className="todos">
+
+
           {toDos.map((obj) => (
-            <div className="todo">
-              <div className="left">
-                <input
-                  onChange={(e) => {
-                    setToDos(
-                      toDos.map((objafter) => {
+          obj.state &&
+            <div className="todos">
+              <div className="todo">
+                <div className="left">
+                  <p>{obj.text}</p>
+                </div>
+
+              < div className="right">
+      {/* < ------------***** TODOS LIST *****---------- > */}
+
+          {/* < ------ COMPLETED BUTTON ------- > */}
+                <div className="inputs" style={{backgroundColor:'lightgreen'}}>
+                  <label htmlFor="checkbox">COMPLETED</label>
+                  <input
+                    onChange={(e) =>{
+                      setToDos(toDos.filter(objafter =>{
                         if (objafter.id === obj.id) {
-                          objafter.tick = e.target.checked;
+                        objafter.cm = e.target.checked;
+                        objafter.state = !e.target.checked;
+                        objafter.time =datetime
                         }
                         return objafter;
                       })
-                    );
-                  }}
-                  value={obj.tick}
-                  type="checkbox"
-                  name=""
-                  id=""
+                      );
+                  
+                    }}
+                    value={obj.tick} 
+                    type="checkbox"
+                    name="checkbox"
                   />
-                <p>{obj.text}</p>
               </div>
-              <div className="right">
-              <i 
-            class="fa-sharp fa-solid fa-trash"></i>
+                {/* < ------**** COMPLETED BUTTON END  ****------- > */}
+
+          {/* < ---------------***** CREATE TODOS ENDS ****------------- > */}
+
+
+                {/* < ------------ DELETE BUTTON ---------- > */}
+      <div className="inputs" style={{backgroundColor:'red'}}>
+                  <label htmlFor="">DELETE</label>
+              
+              <input   
+                 onChange={(e)=>{setToDos(
+                          toDos.filter(delobj => {
+                          if (delobj.id === obj.id) {
+                          delobj.del = !e.target.checked;
+                          delobj.time=datetime;
+                          delobj.state=false
+                          }
+                          return delobj;
+                      })
+                    );}}
+                    type="checkbox" 
+              />
               </div>
             </div>
-          ))}
-        </div>
-        <div className="active">
-          <h1>
-            <u>Active task</u>
-          </h1>
-          {toDos.map((obj) => {
-            if (obj.tick) {
-              return (
-                <div>
-                  <p>
-                    task : [{obj.text}] TaskId : {obj.id}
-                  </p>
-                </div>
-              );
-            }
-            return null;
-          })}
-        </div>
+          </div>
+          </div>
+          ))} 
+
+        
       </div>
-    </div>
+                    </div>
+        {/* < ------------**** CREATE TODOS ENDS **** ---------- > */}
+
+
+
+                    {/* < ----- COMPLETED TODOS ---- > */}
+
+      <div className="card" style={{overflowY:"scroll",overflowX:'hidden'}}>
+        
+        <div className="active">
+          <h2>
+            COMPLETED TODOS ✅
+          </h2>
+            {toDos.map((obj) => 
+              obj.cm &&
+              
+                <div className="deleted" style={{backgroundColor:'lightgreen'}}>
+
+                  <p>task : {obj.text} {obj.time}</p>
+
+                </div>
+            )}
+        </div>
+        
+      </div>
+          {/* < -----***** COMPLETED TODOS ENDS ******---- > */}
+
+
+              {/* < ---------- DELETED TODOS ---------- > */}
+
+                    <div className="card" style={{overflowY:"scroll",overflowX:'hidden'}}>
+                        <h2 style={{backgroundColor:"grey" ,padding:'0%'}}>DELETED TODOS ❌</h2>
+                     
+                        {toDos.map((obj)=>
+                          obj.del? "" :
+
+                        <div className="deleted">
+                          
+                          <div><p style={{color:'white'}}>{obj.text}</p><p>{obj.time}</p></div>
+                          <div >
+                            <span><p style={{color:'blue'}}>Restore</p></span>
+                            <input   
+                              onChange={(e)=>{setToDos(toDos.filter(delobj =>{
+                              if (delobj.id === obj.id) {
+                              delobj.del = e.target.checked;
+                              delobj.state=true
+                              }
+                              return delobj;
+                              })
+                              )}}
+                              type="radio"
+                            class="fa-sharp fa-solid fa-rotate-right fa-spin"
+                            />
+
+                          </div>
+                        </div>
+                     
+                      )}
+                    </div>
+    {/* < ----------***** DELETED TODOS ENDS *****---------- > */}
+      </div> 
+</div>
+    
     );
 }
 
